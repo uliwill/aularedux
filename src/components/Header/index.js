c6848e906b6fd15230213b1d9f6bcde2aa8ed790
Ulisses Willian Reichenbach <uliwill@gmail.com>
2020-02-28 19:42:16 -0300

    Estilizando Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.svg';
+import './style.css';
 
 export default function Header() {
- return (
-   <header>
-       <h1>Header</h1>
-   </header>
- );
+  return (
+    <header className="container">
+      <Link to="/">
+        <img className="logo" src={logo} alt="Logo do Projeto" />
+      </Link>
+      <Link className="reserva" to="/reservas">
+        <div>
+          <strong>Minhas reservas</strong>
+          <span>0 reservas</span>
+        </div>
+      </Link>
+    </header>
+  );
 }
